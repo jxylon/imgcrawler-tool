77f34941b993b6daf5ec4c0b20200cd235d79734
jxylon <409767510@qq.com>
2023-06-06 00:48:47 +0800

    Git init again

---
+++ imgcrawler/main.py
@@ -0,0 +1,11 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+import crawler_ui
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    MainWindow = QMainWindow()
+    ui = crawler_ui.Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
